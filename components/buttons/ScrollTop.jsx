e145b1343112d20a31edd60b8ec82e831fd2fd39
Shiva <shivaxwarrior@gmail.com>
2023-05-14 16:39:25 +0530

    Fixe scroll top jsx

--- components/buttons/ScrollTop.jsx
+++ components/buttons/ScrollTop.jsx
@@ -1,33 +1,37 @@
-
 import styles from "./ScrollTop.module.scss";
 import { useState, useEffect } from "react";
-import {AiOutlineArrowUp} from "react-icons/ai"
+import { AiOutlineArrowUp } from "react-icons/ai";
+
 export const ScrollTop = () => {
-    const [isVisible, setIsVisible] = useState(false)
-    const goToBtn = () => {
-        window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+  const [isVisible, setIsVisible] = useState(false);
+
+  const goToBtn = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  const listenToScroll = () => {
+    let hidden = 800;
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    if (winScroll > hidden) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
     }
+  };
 
-    const listenToScroll = () => {
-        let hidden = 800
-        const winScroll =
-            document.body.scrollTop || document.documentElement.scrollTop;
-        if (winScroll > hidden) {
-            setIsVisible(true)
-        } else {
-            setIsVisible(false)
-        }
-    };
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener("scroll", listenToScroll);
-        return () => window.removeEventListener("scroll", listenToScroll);
-    }, [])
-    return (
-        isVisible && (
-            <button onClick={goToBtn} className={styles.scroll}>
-                <AiOutlineArrowUp/>
-            </button>
-        )
-    );
+  return (
+    <div className={styles["scroll-wrapper"]}>
+      {isVisible && (
+        <button onClick={goToBtn} className={styles.scroll}>
+          <AiOutlineArrowUp />
+        </button>
+      )}
+    </div>
+  );
 };
