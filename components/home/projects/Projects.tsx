cbdb6064d7607de7466e11ce963014260e04116d
Shiva <shivaxwarrior@gmail.com>
2023-05-14 17:25:21 +0530

    Changed index

--- components/home/projects/Projects.tsx
+++ components/home/projects/Projects.tsx
@@ -48,8 +48,8 @@ const projects = [
   {
     title: "Vermilion",
     imgSrc: "/project-imgs/portfolio.png",
-    code: "https://github.com/ShivaBhattacharjee/Griffith-WhatsappBot",
-    projectLink: "",
+    code: "https://github.com/ShivaBhattacharjee/vermilion-a-portfolio-website",
+    projectLink: "https://immashiva.vercel.app/",
     tech: ["TypeScript","NextJs", "EmailJs","Framer-Motion", "SCSS"],
     description:
       "Vermilion is a stunning portfolio template designed to showcase your creative work with style and sophistication. Built using the cutting-edge Next.js framework, this template offers lightning-fast page loading times and a smooth user experience.",
@@ -59,16 +59,10 @@ const projects = [
         Vermilion is an exceptional portfolio template designed to showcase your creative work in a stunning and dynamic way. The template is built using TypeScript, which provides static type checking and helps ensure your code is robust and error-free.
         </p>
         <p>
-        <p>
         Vermilion is built on the Next.js framework, which offers many benefits such as fast page loading times and server-side rendering. This ensures that your portfolio website is performant, responsive, and delivers an excellent user experience.
         </p>
-        The SCSS preprocessor is used for styling, enabling you to create customized styles that perfectly match your brand and personality. The result is a beautiful and modern design that is both eye-catching and user-friendly.
-        </p>
         <p>
-        The template also integrates with EmailJS, a simple and reliable email delivery service that allows you to send messages via the contact form. This ensures that your website visitors can easily get in touch with you, providing an easy way to connect and collaborate on new projects.
-        </p>
-        <p>
-        The Framer Motion library is used for animations, enabling you to create engaging and visually appealing transitions and effects that bring your portfolio to life. Additionally, the AnimeJS library is utilized to create grid animations that add an extra layer of depth and interactivity to your website.
+        The template also integrates with EmailJS, a simple and reliable email delivery service that allows you to send messages via the contact form.
         </p>
         <p>
           Overall, Vermilion is an outstanding portfolio template that combines the latest technologies and libraries to deliver a modern, professional, and highly effective website for showcasing your creative work.
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ export default function home() {
   return (
     <>
       <Head>
-        <title>Shiva | Idk nigga</title>
-        <meta name="description" content="Bleep boop" />
+        <title>Shiva</title>
+        <meta name="description" content="I am a full stack web developer experienced in mern stack " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
