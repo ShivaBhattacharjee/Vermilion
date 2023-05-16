e40c3f9412c6cb88b8c6ae325bc60285962bc5d2
Shiva <shivaxwarrior@gmail.com>
2023-05-16 16:00:34 +0530

    changed favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function home() {
         <title>Shiva | Building beautiful, functional websites for your business</title>
         <meta name="description" content="I am a full stack web developer experienced in mern stack " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/shivalogo.ico" />
       </Head>
       <Home />
     </>
